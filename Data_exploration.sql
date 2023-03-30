cff961f132f34d2efa0e80be7cb9347b862b0f10
LukeStarnes <123986345+LukeStarnes@users.noreply.github.com>
2023-03-30 17:30:21 +0100

    Create Data_exploration.sql

---
+++ Data_exploration.sql
@@ -0,0 +1,13 @@
+-- Average overall, member and casual ride lengths
+
+SELECT
+(SELECT AVG(ride_length)
+FROM yearly_data) AS AvgRideLength_Overall,
+
+(SELECT AVG(ride_length)
+FROM yearly_data
+WHERE member_casual = 'member') AS AvgRideLength_Member,
+        
+(SELECT AVG(ride_length)
+FROM yearly_data
+WHERE member_casual = 'casual' ) AS AvgRideLength_Casual
