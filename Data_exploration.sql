009c816a983f90614b218b25250e859e2f64babc
Luke Starnes <123986345+LukeStarnes@users.noreply.github.com>
2023-04-02 00:32:01 +0100

    Update Data_exploration.sql

--- Data_exploration.sql
+++ Data_exploration.sql
@@ -1,14 +1,18 @@
--- Average overall, member and casual ride lengths
+-- Average overall, member and casual ride lengths by month
 
-SELECT
-
-(SELECT AVG(ride_length)
-FROM yearly_data) AS AvgRideLength_Overall,
-
-(SELECT AVG(ride_length)
+SELECT ride_month, AVG(ride_length) AS Total_Average
 FROM yearly_data
-WHERE member_casual = 'member') AS AvgRideLength_Member,
+GROUP BY ride_month
+-- Total varies from 11:30mins in December to 19:52 in May
+
+SELECT ride_month, AVG(ride_length) AS Member_Average
+FROM yearly_data
+GROUP BY ride_month, member_casual
+HAVING member_casual = 'member'
+-- Total varies from 10:25mins in December to 13:56 in May
         
-(SELECT AVG(ride_length)
+SELECT ride_month, AVG(ride_length) AS Casual_Average
 FROM yearly_data
-WHERE member_casual = 'casual' ) AS AvgRideLength_Casual
+GROUP BY ride_month, member_casual
+HAVING member_casual = 'casual'
+-- Total varies from 15:02mins in December to 27:59 in May
