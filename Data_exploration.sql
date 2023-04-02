b91496893c88e756af7dff3c79c9e518c02382f8
Luke Starnes <123986345+LukeStarnes@users.noreply.github.com>
2023-04-02 03:01:57 +0100

    Update Data_exploration.sql

--- Data_exploration.sql
+++ Data_exploration.sql
@@ -18,6 +18,7 @@ HAVING member_casual = 'casual'
 -- Total varies from 15:02mins in December to 27:59 in May
 
 
+
 -- Total trips by rider type with percentage
 WITH t1 AS
 (
@@ -36,6 +37,7 @@ FROM t1
 -- Members make up 60% of total riders, casuals make up 40%
 
 
+
  -- Looking at total number of trips, member trips and casual trips per day 
 SELECT day_of_week, COUNT(DISTINCT ride_id) AS TotalTrips,
 COUNT(*) FILTER(WHERE member_casual = 'member') AS MemberTrips,
@@ -46,3 +48,17 @@ ORDER BY TotalTrips DESC
 --Here we can see clearly that members outweight casuals on weekdays. 
 --Saturday is the most popular day and surprisingly Sunday is 6th.
 --This will be good to visualise
+
+
+
+-- Looking at total start station counts
+SELECT DISTINCT start_station_name,
+SUM(CASE WHEN ride_id = ride_id AND start_station_name = start_station_name THEN 1 ELSE 0 END) AS total,
+SUM(CASE WHEN member_casual = 'member' AND start_station_name = start_station_name THEN 1 ELSE 0 END) AS member,
+SUM(CASE WHEN member_casual = 'casual' AND start_station_name = start_station_name THEN 1 ELSE 0 END) AS casual
+FROM yearly_data
+GROUP BY start_station_name
+ORDER BY total DESC
+-- Some interesting patterns in the start station data are emerging, indicating that different stations are preferred by casual riders and annual members
+-- By changing the order by to member or casual, its clear to see that memebrs use different stations to casual riders. It looks like Casual riders use stations
+-- close to the water. Only one station (Wells St & Concord Ln) appears in both the members and casuals top 10 start station list. This will be interesting to visualise.
